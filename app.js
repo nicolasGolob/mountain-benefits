239c2e35f14b1c510769fa0d8a7820265482db13
Nicolas Golob <nicolasgolob@gmail.com>
2021-06-06 08:05:10 +0200

    animation created and adding content

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-
 let TL = new TimelineMax();
-
 TL.to(".moonlight", 1, {y:+30 ,autoAlpha:1})
 TL.to(".main-title", 1, {x:-30 ,autoAlpha:1})
 .to(".valley-bottom", 1,{y:-100 ,autoAlpha:1}, "-=0.1")
 .to(".mountain-left", 1,{y:-100 ,autoAlpha:1}, "-=0.1")
 .to(".mountain-right", 1,{y:-100 ,autoAlpha:1}, "-=0.1")
 
+/* content **/
+const contents = gsap.utils.toArray('.content-text');
+contents.forEach((content) => {
+  gsap.to(content, { duration:1, y:0, autoAlpha: 1,
+    scrollTrigger: {
+        trigger: content,
+        start: "top 95%",
+        end: "bottom 30%",
+        toggleActions : "play",
+        scrub: true    }
+    });
+})
 
 
